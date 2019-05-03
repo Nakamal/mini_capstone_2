ee41555287c0af96c242759c21a36597e4e531b8
Nakamal <nickhaugland@gmail.com>
2019-05-03 10:25:07 -0500

    added renders for all restful routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,22 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.json.jbuilder"
+  end
+
+  def create
+    render "show.json.jbuilder"
+  end
+
+  def show
+    render "show.json.jbuilder"
+  end
+
+  def update  
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    
+  end
 end
